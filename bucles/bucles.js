f00ff5c147f750220b31f858806518415bfb4943
avanegas <arco527@gmail.com>
2024-03-04 15:19:26 -0500

    Add bucles file

---
+++ bucles/bucles.js
@@ -0,0 +1,23 @@
+// for, sabemos la cant max exacta de pasos que queremos ejecutar
+
+/* var suma = 0
+
+for (var i=0; i<10; i++){
+  suma = suma +1;
+
+
+  console.log('Variable de iteración', i)
+  console.log('---------------------')
+ 
+}
+
+console.log('Variable de suma', suma)
+
+*/
+
+// while: permite repetir una condición siempre y cuando una acción sea verdadera
+// no sabemos con certeza cuantos pasos necesitaremos hasta finalizar la ejecución
+while(suma < 3){
+    suma = su+1
+}
+
